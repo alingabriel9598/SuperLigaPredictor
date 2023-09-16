1d9c809e91686dc89e21fa1a2ffc2f5a835a9c7b
Alin Gabriel <alingabriel9598@gmail.com>
2023-09-16 16:10:01 +0300

    Added 2 variables for title and current date, also logging them

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Test');
+const title = 'SuperLiga Predictor - Etapa 9 Sezonul 23/24';
+const date = new Date();
+console.log(title);
+console.log(`${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`);
